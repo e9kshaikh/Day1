219942a750f72c87954e0cbc13a0e3c4c8f8dcb3
e9kshaikh <khizar.shaikh@enine.school>
2023-12-16 14:03:00 +0530

    Changes of Blacked Formatting

--- sample.py
+++ sample.py
@@ -1,15 +1,17 @@
 # Method Number 1
-def gettotal(a,b):
-    total_sum=sum(range(a,b))
-    return(total_sum)
+def gettotal(a, b):
+    total_sum = sum(range(a, b))
+    return total_sum
 
-print(gettotal(4212,18912512+1))
 
 # Method Number 2
-def gettotalmethod2(a,b):
-    total=0
-    for num in range(4212,18912512+1):
-        total+=num
+def gettotalmethod2(a, b):
+    total = 0
+    for num in range(a, b + 1):
+        total += num
     return total
 
-print(gettotalmethod2(4212,18912512+1))
+
+if __name__ == "__main__":
+    print(f"{gettotal(4212, 18912512 + 1)}")
+    print(f"{gettotalmethod2(4212, 18912512 + 1)}")
