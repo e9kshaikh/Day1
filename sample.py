8cf0d9a4de6aeb9d8048449640e3af147536b009
e9kshaikh <khizar.shaikh@enine.school>
2023-12-17 02:11:35 +0530

    changes of Pylint

--- sample.py
+++ sample.py
@@ -1,11 +1,21 @@
+"""
+This script defines two methods for calculating the sum of numbers within a specified range.
+"""
+
 # Method Number 1
 def gettotal(a, b):
+    """
+    Calculates the sum of numbers in the range (a, b).
+    """
     total_sum = sum(range(a, b))
     return total_sum
 
 
 # Method Number 2
 def gettotalmethod2(a, b):
+    """
+    Calculates the sum of numbers in the range (a, b).
+    """
     total = 0
     for num in range(a, b + 1):
         total += num
